187c61145bab594ea71baa1415804c3aac970467
Yung-DP <134976162+Yung-DP@users.noreply.github.com>
2023-10-17 17:21:59 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -47,6 +47,7 @@ if (isset($_POST["submit"])) {
         $uploadOk = 0;
     }
 
+    
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
         echo "Sorry, your file was not uploaded.";
